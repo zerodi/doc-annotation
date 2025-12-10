8bf8481c26cf6a600a1963fbaa574e3b9d61700f
Dmitriy Pyankov <mail@zerodi.ru>
2025-12-10 18:21:54 +0500

    add main providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,17 @@
+import { provideEventPlugins } from '@taiga-ui/event-plugins';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAnimations(),
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideHttpClient(),
+    provideEventPlugins(),
+  ],
 };
